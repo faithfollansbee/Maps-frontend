7b541afa28eece7dcda4c3fc87c34d8c5e3e9a86
faithfollansbee <faithfollansbee@gmail.com>
2020-01-14 17:54:09 -0500

    Fixed production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,9 +1,9 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://agile-reaches-15888.herokuapp.com',
   development: 'http://localhost:4741'
 }
-
+// https://aqueous-atoll-85096.herokuapp.com
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
 } else {
