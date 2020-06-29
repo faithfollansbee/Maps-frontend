902229be38bccbe21d898d421de2ab6d268db122
faithfollansbee <faithfollansbee@gmail.com>
2020-06-29 12:47:23 -0400

    Added card to sign in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+
 import { signIn } from '../../api/auth'
 import messages from '../AutoDismissAlert/messages'
-
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const signInStyle = {
-  background: 'white',
-  margin: '15px',
-  padding: '10px'
-}
+// const signInStyle = {
+//   background: 'white',
+//   margin: '15px',
+//   padding: '10px'
+// }
 class SignIn extends Component {
   constructor () {
     super()
@@ -55,38 +57,42 @@ class SignIn extends Component {
 
     return (
       <div className="row">
-        <div className="col-sm-10 col-md-8 mx-auto mt-5" style={signInStyle}>
-          <h3>Sign In</h3>
-          <Form onSubmit={this.onSignIn}>
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                required
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Enter email"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                required
-                name="password"
-                value={password}
-                type="password"
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-            <Button
-              type="submit"
-              variant="dark"
-            >
-              Submit
-            </Button>
-          </Form>
+        <div className="col-sm-10 col-md-8 mx-auto mt-5">
+          <Card variant="outlined dark">
+            <CardContent>
+              <h3>Sign In</h3>
+              <Form onSubmit={this.onSignIn}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    required
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="Enter email"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    required
+                    name="password"
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+                <Button
+                  type="submit"
+                  variant="dark"
+                >
+                  Submit
+                </Button>
+              </Form>
+            </CardContent>
+          </Card>
         </div>
       </div>
     )
