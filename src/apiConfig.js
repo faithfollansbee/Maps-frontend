a0150c8b0acf9f2576b941ca0037a5e7f2009f51
faithfollansbee <faithfollansbee@gmail.com>
2023-02-19 18:44:46 -0500

    apiUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,7 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://agile-reaches-15888.herokuapp.com',
+  production: 'https://maps-app-04gr.onrender.com',
+  // production: 'https://agile-reaches-15888.herokuapp.com',
   development: 'http://localhost:4741'
 }
 // https://aqueous-atoll-85096.herokuapp.com
